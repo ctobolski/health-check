30bc70ec6ce9ea0e6c8a9ce6c7805437063020c1
Chris Tobolski <christopher.tobolski@gmail.com>
2018-12-14 19:27:43 -0500

    Testing hook using pretty-quick instead of lint-staged

--- src/components/AppStatus.js
+++ src/components/AppStatus.js
@@ -1,42 +1,42 @@
-import React, {Component} from 'react'
-import axios from "axios";
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class AppStatus extends Component {
   constructor(props) {
-    super(props);
-    this.state = { status: "unknown", loading: true };
+    super(props)
+    this.state = { status: 'unknown', loading: true }
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
+    clearInterval(this.timer)
   }
 
   componentDidMount() {
-    this.checkHealth();
-    this.timer = setInterval(() => this.checkHealth(), this.props.interval);
+    this.checkHealth()
+    this.timer = setInterval(() => this.checkHealth(), this.props.interval)
   }
 
   checkHealth() {
-    this.setState({ loading: true });
+    this.setState({ loading: true })
     axios
       .get(this.props.url)
       .then(({ data }) => {
-        this.setState({ status: "up" });
+        this.setState({ status: 'up' })
       })
       .catch(error => {
-        this.setState({ status: "down" });
+        this.setState({ status: 'down' })
       })
       .finally(() => {
-        this.setState({ loading: false });
-      });
+        this.setState({ loading: false })
+      })
   }
 
   render() {
-    const { status, loading } = this.state;
+    const { status, loading } = this.state
     return (
-      <div className={`app ${status} ${loading ? "loading" : ""}`}>
+      <div className={`app ${status} ${loading ? 'loading' : ''}`}>
         <h1>{this.props.name}</h1>
       </div>
-    );
+    )
   }
 }
