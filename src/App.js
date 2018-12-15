bb2db907d8b8ef14295c3ddd0b85d234f81332f6
Chris Tobolski <christopher.tobolski@gmail.com>
2018-12-14 19:19:38 -0500

    adding formatter and commit hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Fragment } from "react";
-import "./App.css";
-import AppStatus from "./components/AppStatus";
+import React, { Fragment } from 'react'
+import './App.css'
+import AppStatus from './components/AppStatus'
 
 const App = ({ environments }) => {
   return (
     <Fragment>
       {environments.map(({ name, apps }, index) => {
-        return <Environment name={name} apps={apps} key={index} />;
+        return <Environment name={name} apps={apps} key={index} />
       })}
     </Fragment>
-  );
-};
+  )
+}
 
 const Environment = ({ name, apps }) => {
   const renderApps = apps => {
@@ -22,17 +22,16 @@ const Environment = ({ name, apps }) => {
           interval={app.interval}
           key={index}
         />
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
-    <div className="environment">
-      <div className="title">{name}</div>
-      <div className="row">{renderApps(apps)}</div>
+    <div className='environment'>
+      <div className='title'>{name}</div>
+      <div className='row'>{renderApps(apps)}</div>
     </div>
-  );
-};
+  )
+}
 
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { environments } from "./config.js";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { environments } from './config.js'
 
-ReactDOM.render(<App environments={environments} />, document.getElementById('root'));
-
-
+ReactDOM.render(
+  <App environments={environments} />,
+  document.getElementById('root')
+)
